16f18676fe9c3d09622d2373ecdc56ded277318a
koncpa <pkoncity@redhat.com>
2024-04-02 08:37:47 +0200

    Rename export script

--- Setup/install_upstream_operator/runtest.sh
+++ Setup/install_upstream_operator/runtest.sh
@@ -39,7 +39,7 @@ rlJournalStart
             rlRun "pushd /var/tmp/tang-operator_sources"
             CONTROLLER_IMG="tang-controller"
             OP_BUNDLE_IMG="tang-operator-bundle"
-            rlRun "sed -i 's/FROM golang:1.21 as builder/FROM docker.io\/library\/golang:1.21 as builder/g' Dockerfile"
+            rlRun "sed -i 's/FROM golang:1.22 as builder/FROM docker.io\/library\/golang:latest as builder/g' Dockerfile"
             #Need to export to use it in make build commands
             export IMG="${IP}:5000/${CONTROLLER_IMG}:latest"
             export BUNDLE_IMG="${IP}:5000/${OP_BUNDLE_IMG}:latest"
@@ -49,7 +49,7 @@ rlJournalStart
             rlRun "pushd /var/tmp/attestation-operator_sources"
             CONTROLLER_IMG="attestation-controller"
             OP_BUNDLE_IMG="attestation-operator-bundle"
-            rlRun "sed -i 's/FROM golang:1.20 as builder/FROM docker.io\/library\/golang:1.20 as builder/g' build/docker/attestation-operator/Dockerfile"
+            rlRun "sed -i 's/FROM golang:1.22 as builder/FROM docker.io\/library\/golang:1.20 as builder/g' build/docker/attestation-operator/Dockerfile"
             #Need to export to use it in make build commands
             export DOCKER_TAG="${IP}:5000/${CONTROLLER_IMG}:latest"
             export BUNDLE_IMG="${IP}:5000/${OP_BUNDLE_IMG}:latest"
@@ -63,11 +63,11 @@ EOF
         rlRun "podman container run -dt -p 5000:5000 --name registry --volume registry:/var/lib/registry:Z docker.io/library/registry:2"
         #Check if it's registry accesible
         rlRun "curl ${IP}:5000/v2/_catalog" 0 "Checking registry availability"
-        rlRun "make podman-build podman-push"
-        rlRun "make bundle"
+        rlRun "make podman-build podman-push" 0,2 "Panic error when go build package"
+        rlRun "make bundle" 0,2 "Panic error when go build package"
         rlRun "make podman-bundle-build podman-bundle-push"
         #For another shell sessions
-        cat <<EOF > /etc/profile.d/upstream_tang_init.sh
+        cat <<EOF > /etc/profile.d/upstream_operator_init.sh
 #!/bin/bash
 export IMAGE_VERSION=${BUNDLE_IMG}
 export RUN_BUNDLE_PARAMS="--use-http"
